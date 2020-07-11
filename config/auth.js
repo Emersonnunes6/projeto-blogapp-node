596909713ba4720c5333611db2d5cfb89f5ddf72
unknown <enunessena@gmail.com>
2020-07-11 20:29:03 -0300

    make it better

--- config/auth.js
+++ config/auth.js
@@ -5,7 +5,7 @@ const passport = require("passport")
 
 //Model de usuário
 require("../models/usuario")
-const Usuario = mongoose.model("usuario")
+const Usuario = mongoose.model("usuario", Usuario)
 
 module.exports = function(passport){
 
