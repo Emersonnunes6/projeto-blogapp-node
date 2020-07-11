cf59b9ab96d9d8d36f3e3d0714b4d564d89ad013
unknown <enunessena@gmail.com>
2020-07-11 18:03:53 -0300

    oooo

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI:"mongodb+srv://emersonnunes6:<password>@blogapp-prod.debyz.mongodb.net/test"}
+    module.exports = {mongoURI:"mongodb+srv://emersonnunes6:emersonnunes123@blogapp-prod.debyz.mongodb.net/blogapp-prod?retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoURI: "mongodb://localhost/blogapp"}
 }
