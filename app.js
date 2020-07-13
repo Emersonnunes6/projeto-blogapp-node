07c29536fb29c403c57a0766d3fe8886876bd86c
unknown <enunessena@gmail.com>
2020-07-13 11:21:49 -0300

    c

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const db = require("./config/db")
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars');
     //Mongoose
+    console.log('link de conexão: ' + db.mongoURI')
     mongoose.Promise = global.Promise;
         mongoose.connect(db.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true}).then(function(){
             console.log("Conectado ao mongo")
